1cad5cfe883594ae664cc7619637fd6bece07068
One290 <lenya.dorovskoy@mail.ru>
2024-02-07 19:51:53 +0800

    Work

--- Weather.Core/Domain/Weather.cs
+++ Weather.Core/Domain/Weather.cs
@@ -1,6 +1,6 @@
 namespace Weather.Core.Domain;
 
-public class Weather
+public class Weather : IDisposable
 {
     public Weather(Uri rootUrl, string token, IHttpClientFactory httpClientFactory)
     {
@@ -8,6 +8,20 @@ public class Weather
         _httpClient.BaseAddress = rootUrl;
         _httpClient.DefaultRequestHeaders.Add("X-Gismeteo-Token", token);
     }
+
+    public string GetWeather(double latitude, double longitude)
+    {
+        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180)
+            throw new Exception("Latitude and longitude values must lie in the intervals [-90;90] and" +
+                                "[-180;180] respectively");
+        
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get,
+            $"?latitude={latitude}&longitude={longitude}");
+        
+        
+    }
+    
+    public void Dispose() => _httpClient.Dispose();
 
     private HttpClient _httpClient;
 }
--- Weather.Web/Program.cs
+++ Weather.Web/Program.cs
@@ -1,3 +1,7 @@
+using System.Globalization;
+
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
