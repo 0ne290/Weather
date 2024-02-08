b7392818329d5375e4c4562015f70a91369413ae
Алексей <97665299+0ne290@users.noreply.github.com>
2024-02-08 22:06:23 +0800

    Work

---
+++ Weather.Core/Application/GismeteoService.cs
@@ -0,0 +1,27 @@
+using Newtonsoft.Json;
+using Weather.Core.Application.Dtos;
+
+namespace Weather.Core.Application;
+
+public class GismeteoService : IDisposable
+{
+    public GismeteoService(Domain.ExternalApi gismeteo) => _gismeteo = gismeteo;
+    
+    public async Task<GismeteoWeatherGeneralDto> GetGeneralWeatherAsync(double latitude, double longitude)
+    {
+        var serializedJson = await _gismeteo.SendAsync($"latitude={latitude}&longitude={longitude}");
+
+        var z = new JsonSerializerSettings
+        {
+            DateFormatString = "yyyy'-'MM'-'dd' 'HH':'mm':'ss"
+        }
+        
+        var definition
+        
+        var deserializedJson = JsonConvert.DeserializeAnonymousType();
+    }
+    
+    public void Dispose() => _gismeteo.Dispose();
+
+    private readonly Domain.ExternalApi _gismeteo;
+}
