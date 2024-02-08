4694f0f5d6c9bc2717bab57d79d0a5a83990e922
Алексей <97665299+0ne290@users.noreply.github.com>
2024-02-08 23:09:16 +0800

    Update GismeteoService.cs

--- Weather.Core/Application/GismeteoService.cs
+++ Weather.Core/Application/GismeteoService.cs
@@ -7,8 +7,12 @@ public class GismeteoService : IDisposable
 {
     public GismeteoService(Domain.ExternalApi gismeteo) => _gismeteo = gismeteo;
     
-    public async Task<GismeteoWeatherGeneralDto> GetGeneralWeatherAsync(double latitude, double longitude)
+    public async Task<GismeteoWeatherGeneralDto> GetGeneralWeatherAsync(double latitude, double longitude)// Код вызова фабрики мб лучше расположить тут
     {
+        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180)
+            throw new Exception("Latitude and longitude values must lie in the intervals [-90;90] and" +
+                                "[-180;180] respectively");
+    
         var serializedJson = await _gismeteo.SendGetAsync($"latitude={latitude}&longitude={longitude}");
 
         var z = new JsonSerializerSettings
